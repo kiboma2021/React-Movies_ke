579803d8a314de3a12ad68e02f7aa49246f67961
kiboma <omayioben@gmail.com>
2024-01-12 08:57:47 +0300

    Use flowbite for my header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,29 @@
+import { Link } from "react-router-dom"
+
 export const Footer = () => {
   return (
-    <footer>
-        <span>@2030 Cineflix. All Rights Reserved</span>
-        <ul>
-            <li><a href=""> Instagram </a></li>
-            <li><a href=""> LinkedIn </a></li>
-            <li><a href=""> Twitter </a></li>
-            <li><a href=""> YouTube </a></li>
+
+    <footer className="bg-white m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">Flowbite™</Link>. All Rights Reserved.
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+            </li>
+            <li>
+                <a href="#" target="_blank" className="hover:underline me-4 md:me-6">LinkedIn</a>
+            </li>
+            <li>
+                <a href="#" target="_blank" className="hover:underline me-4 md:me-6">Twitter</a>
+            </li>
+            <li>
+                <a href="#" target="_blank" className="hover:underline">YouTube</a>
+            </li>
         </ul>
-      
+        </div>
     </footer>
+
   )
 }
 
