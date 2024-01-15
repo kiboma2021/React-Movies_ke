3001d28cfd35b67e442eed7652a66545020d1471
kiboma <omayioben@gmail.com>
2024-01-15 17:16:34 +0300

    Fetch API

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom'
-import MovieImg from '../assets/action.jpeg'
 
 export const Card = ({movie}) => {
   const {id,title,overview,poster_path }=movie;
   const image = `https://image.tmdb.org/t/p/w500/${poster_path}`;
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <Link to="#">
+      <Link to={`/movie/${id}`}>
           <img className="rounded-t-lg" src={image} alt="Movie" />
       </Link>
       <div className="p-5">
-          <Link to="#">
+          <Link to={`/movie/${id}`}>
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
           </Link>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
