221e2633497560dada6e853fe44a3e1723d5bb82
kiboma <omayioben@gmail.com>
2024-01-15 18:20:13 +0300

    Concele API key

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,7 +3,7 @@ import { useState,useEffect } from "react";
 
 export const useFetch = (apiPath) => {
     const [ data, setData ] = useState([]);
-    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=e08fdaedf1ced595a205509bab8db76d`;
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}`;
 
 
     useEffect(()=> {
