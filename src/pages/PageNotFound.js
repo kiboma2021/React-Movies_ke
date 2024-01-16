144b3e88234c3b72c9c21fa1c46efe256aa43727
kiboma <omayioben@gmail.com>
2024-01-16 08:03:30 +0300

    Add page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,18 @@
+import { Button } from "../components"
+import pageNotFound from "../assets/pageNotFound.jpeg"
+
 export const PageNotFound = () => {
   return (
     <main>
-        <h2>Page not found</h2>
+        <section className="flex flex-col justify-center">
+          <div className="flex flex-col max-w-2xl">
+            <p className="flex justify-center">Error 404, Page not found</p>
+            <img src={pageNotFound} alt="Error 404!!!" />
+          </div>
+          <div className="flex justify-center">
+            <Button>Back to Home page</Button>
+          </div>
+        </section>
       
     </main>
   )
